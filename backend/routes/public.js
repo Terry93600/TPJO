c8cc477bb1f93bf592a13d17ca39a964da939d38
jaouanii <ijaouani.pro@gmail.com>
2023-05-30 14:47:17 +0200

    fichiers backend / express

---
+++ backend/routes/public.js
@@ -0,0 +1,20 @@
+// import
+import express from "express";
+
+// créer le routeur
+const publicRouter = express.Router();
+
+/*
+	créer les routes:
+		- méthode HTTP utilisée
+		- URL de la route
+		- middleware appelé par la route
+*/
+
+// publicRouter.get("/", index);
+
+// creer une route en poste recuperer les donnes| exemple:
+// publicRouter.post('/uneroute', uneroute);
+
+// exporter le routeur
+export default publicRouter;

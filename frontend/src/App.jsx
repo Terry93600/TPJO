f7370eb8088e6f665cfd054d91a8a234658fae6b
Jaouanii <ijaouani.pro@gmail.com>
2023-06-01 22:12:45 +0200

    call data from backend+ afficage sport

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 // import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Accueil from './components/accueil/Accueil';
+import CallSport from './components/callSport';
 import Footer from './components/footer/Footer';
 import Header from './components/my-header/Header.jsx';
 import Sports from './components/sports/Sports';
+
+// import router from './service/Router'
+
 
 function App() {
   
@@ -11,10 +16,17 @@ function App() {
 
   return (
     <>
+      {/* les element suivant seront presents dans toutes les routes */}
       <Header/>
       <Accueil/>
       <Sports/>
-      <Footer/>      
+      <Footer />
+      
+      {/* ici on a creer la route et son chemin pour acceder aux donnees du backend qu'on a stocker dans un fichier React et qu'on importe ici :  */}
+      <Routes>
+        <Route path="/" element={<CallSport/> } />
+     </Routes>
+      
     </>
   )
 }

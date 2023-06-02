4c1bc52489b69f759e917b9429551c801c01608b
Reshmacheck <reshmacheck93600@gmail.com>
2023-06-02 14:18:17 +0200

    back page

--- backend/repositories/epreuvesRepository.js
+++ backend/repositories/epreuvesRepository.js
@@ -5,6 +5,16 @@ const getEpreuve = async () => {
     const sql = `
     SELECT epreuve
     FROM tpjo.epreuve
-    INNER JOIN sport ON sport.id = epreuve.sport_id `
+    INNER JOIN sport ON sport.id = epreuve.sport_id `;
+
+    try {
+        const [results] = await dbConnection.execute(sql);
+        // console.log(results);
+        return results;
+    } catch (error) {
+        // console.log(error);
+        return error;
+    }
+
 }
 export { getEpreuve };

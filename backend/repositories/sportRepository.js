f1f8b0672eecd2a9b0ced035a1db5f2be83779aa
Jaouanii <ijaouani.pro@gmail.com>
2023-06-07 21:53:44 +0200

    ajout image back/front

--- backend/repositories/sportRepository.js
+++ backend/repositories/sportRepository.js
@@ -17,4 +17,22 @@ const getSports = async() => {
     }
 }
 
-export { getSports };
+
+// post d'un sport dans la bdd on utilise insert ainsi que la reprise des noms de nos colonnes(bdd):
+const postSports = async (data) => {
+    const sql = `
+        INSERT INTO tpjo.sport
+        VALUES (NULL, :name, :picto);
+    `;
+
+    try {
+        const [results] = await dbConnection.execute(sql, data);
+        // console.log(results);
+        return results;
+    } catch (error) {
+        // console.log(error);
+        return error;
+    }
+}
+
+export { getSports, postSports };
